bc83e3bdb3aca0e59018c7552fdd20ae71504067
Edgar <a01339939@itesm.mx>
2018-10-09 12:28:42 -0400

    Mapa

--- Dinoland/Ventana.java
+++ Dinoland/Ventana.java
@@ -6,7 +6,7 @@ import java.awt.*;
 
 public class Ventana extends JFrame{
     private JPanel panel1, panel2,panel3,panel4, panel41, panel42, panel43, panel44, panel45, panel46;
-    private JDialog border1, border2, border3, border4;
+    private JDialog border1, border2, border3, border4; 
     private JButton arriba, abajo, derecha, izquierda;
     private JPanel[][] square = new JPanel[5][5];
 
@@ -21,24 +21,26 @@ public class Ventana extends JFrame{
         setSize(800,800);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         
-        panel1 = new JPanel(new FlowLayout());
+	    panel1 = new JPanel();
+        //panel1.setLayout(new FlowLayout());
+	    TitledBorder border1 = new TitledBorder("Mapa");
+        panel1.setBorder(border1);
+        panel1.setLayout(new GridLayout(5,5));
 
         for(int i = 0; i < 5; i++) {
             for(int j = 0; j < 5; j++) {
                 square[i][j] = new JPanel();
                 square[i][j].setLayout(new FlowLayout());
                 square[i][j].setBackground(Color.GREEN);
+                square[i][j].setBorder(BorderFactory.createLineBorder(Color.black));
                 panel1.add(square[i][j]);
             }
             
         }
         add(panel1, BorderLayout.CENTER);
 
-        TitledBorder border1 = new TitledBorder("Mapa");
-        panel1.setLayout(new GridLayout(5,5));
-
-        panel1.setLayout(new FlowLayout());
-        panel1.setBorder(border1);
+        //revalidate
+        //repaint
 
         panel2 = new JPanel();
         TitledBorder border2 = new TitledBorder("Personaje");
@@ -78,12 +80,17 @@ public class Ventana extends JFrame{
         add(panel4);
 
         //Mapa en el segundo panel (Hacer un grid de 5x5 y poner el monito en donde se vaya moviendo con el listener)
+	//panel1.setLayout(new FlowLayout());
 
+
+        //TitledBorder border1 = new TitledBorder("Mapa");
+        
         //Poner imagen fija del mono seleccionado
 
-        //Ir agregando labels dependiendo de lo que pasa
+        //Ir agregando labels dependiendo de lo que pasa Scroll Layout, ir agregando labels
 
         //ImageIcon para los botones, flechas de movimiento en el cuarto panel
+
         ImageIcon abajoImage = new ImageIcon("Desktop/Abajo.png");
         ImageIcon arribaImage = new ImageIcon("Desktop/Arriba.png");
         ImageIcon izquierdaImage = new ImageIcon("Desktop/Izquierda.png");
