4147a47fb202d6cad5778461e9b73ad42ba106d9
Edgar <a01339939@itesm.mx>
2018-10-10 13:27:23 -0400

    Casilla y Heroe

---
+++ Dinoland/Casilla.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.event.*;
+import java.awt.*;
+
+public class Casillas extends JPanel{
+    private Dinosaurio dinosaurio;
+    private Item item;
+    private Heroe heroe;
+
+    public Dinosaurio getDinosaurio(){
+        return Dinosaurio;
+    }
+
+    public void setDinosaurio(Dinosaurio dinosaurio){
+        this.dinosaurio = dinosaurio;
+    }
+
+    public Item getItem(){
+        return Item;
+    }
+
+    public void setItem(Item item){
+        this.item = item;
+    }
+
+    public void Heroe(){
+        return Heroe;
+    }
+
+    public void setHeroe(Heroe heroe){
+        this.heroe = heroe;
+    }
+}
--- Dinoland/Map.java
+++ Dinoland/Map.java
@@ -1,20 +1,6 @@
 public class Mapa{
-    /*private casilla[][] mapa;
-
-    public Mapa(){
-        mapa = new casilla[5][5];
+    public Casillas[][] casillas;
+    public Mapa(int filas, int columnas){
+        casillas = new Casillas[filas][columnas];
     }
-
-    public casilla[] getMapa(){
-		return mochila;
-	}
-    
-    //Llamarlo desde añadir item mochila
-	public void removeItemDeMapa(int i, int j){
-		casilla[i][j]=null;
-    }
-    
-    public void addItemDeMapa(int i, int j){
-        casilla[i][j] = null;//añadir item que se suelta de la mochila
-    }*/
 }
